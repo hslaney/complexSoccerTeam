96deb82f230c18c69e607c6f9aa9932fd0ed0489
hslaney <haley.slaney@gmail.com>
2021-01-21 21:21:59 -0500

    initial

--- src/com/company/Complex.java
+++ src/com/company/Complex.java
@@ -19,18 +19,37 @@ public class Complex {
         return Math.sqrt(a*a + b*b);
     }
 
-    //add 2?
+    //add
     public Complex add(Complex other){
         double ac = this.a + other.a;
         double bd = this.b + other.b;
         return new Complex(ac, bd);
 
     }
+    //two doubles?
+    public Complex add(double a, double b){
+        return this.add(new Complex(a,b));
+    }
 
     //toString
     public String toString(){
-        return "" + a + " + " + b + "*" + "i";//i?
+        return "" + a + " + " + b + "*" + "i";
     }
 
+    //multiply
+    public Complex multiple(Complex other){
+        double ac = this.a * other.a;
+        double bd = this.b * other.b;
+        double first = ac - bd;
 
+        double bc = this.b * other.a;
+        double ad = this.a * other.b;
+        double second = bc+ad;
+
+        return new Complex (first, second);
+    }
+
+    public Complex multiply(double a, double b){
+        return this.multiple(new Complex(a,b));
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,34 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Complex c1 = new Complex(4,3);
-        Complex c2 = new Complex(5,4);
-        System.out.println(c1);
-        System.out.println(c1.abs());
-        System.out.println(c1.add(c2));
+        Complex c2 = new Complex(5,-4);
+        Complex c3 = new Complex(-3, -2);
+        Complex c4 = new Complex(-1,1);
+
+        System.out.println("c1 : " + c1);
+        System.out.println("c2 : " + c2);
+        System.out.println("c3 : " + c3);
+        System.out.println("c4 : " + c4);
+
+        System.out.println();
+
+        System.out.println("c1 abs : " + c1.abs());
+        System.out.println("c2 abs : " + c2.abs());
+        System.out.println("c3 abs : " + c3.abs());
+        System.out.println("c4 abs : " + c4.abs());
+
+        System.out.println();
+
+        System.out.println("c1 + c2 : " + c1.add(c2));
+        System.out.println("c3 + c4 : " + c3.add(c4));
+        System.out.println("c1 + c3 : " + c1.add(-3, -2));
+        System.out.println("c2 + c4 : " + c2.add(-1, 1));
+
+        System.out.println();
+
+        System.out.println("c1 * c2 : " + c1.multiple(c2));
+        System.out.println("c3 * c4 : " + c3.multiple(c4));
+        System.out.println("c1 * c3 : " + c1.multiply(-3, -2));
+        System.out.println("c2 * c4 : " + c2.multiply(-1, 1));
     }
 }
